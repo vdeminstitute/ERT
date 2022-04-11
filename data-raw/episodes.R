5664b6ab54b77b81bfe4101a51cb093b29db427e
Andreas Beger <adbeger@gmail.com>
2022-04-11 15:37:06 +0300

    minor data type fixes

--- data-raw/episodes.R
+++ data-raw/episodes.R
@@ -8,4 +8,8 @@ data(vdem)
 
 episodes <- get_eps()
 
+# Fix some data types
+episodes$country_id <- as.integer(episodes$country_id)
+episodes$year <- as.integer(episodes$year)
+
 usethis::use_data(episodes, overwrite = TRUE)
